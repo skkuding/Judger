9cca73560f8fc314ab66e72d559783ec4cfa77ca
Gyunseo Lee <rbstj0311@naver.com>
2025-02-02 10:23:55 +0000

    fix: fix build script

--- build.sh
+++ build.sh
@@ -1,2 +1,18 @@
-cd /workspaces/Judger
-mkdir build && cd build && cmake .. && make && sudo make install
+#!/bin/bash
+set -ex
+
+# 이전 build 폴더 삭제 (root 소유여도 sudo를 사용)
+if [ -d build ]; then
+  sudo rm -rf build
+fi
+
+# 이전 설치된 output 디렉터리도 삭제 (root 소유일 수 있으므로 sudo 사용)
+if [ -d output ]; then
+  sudo rm -rf output
+fi
+
+mkdir build
+cd build
+cmake ..
+make -j$(nproc)
+sudo make install
